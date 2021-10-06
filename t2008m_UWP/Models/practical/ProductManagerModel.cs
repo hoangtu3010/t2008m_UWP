2437c0d120297f419437c977caab7802643c1662
hoang <hoangtu2378@gmail.com>
2021-10-06 10:45:30 +0700

    Practical

---
+++ t2008m_UWP/Models/practical/ProductManagerModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t2008m_UWP.Models.practical
+{
+    public class ProductManagerModel
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ t2008m_UWP/Pages/practical/ProductManagement.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using t2008m_UWP.Models.practical;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace t2008m_UWP.Pages.practical
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ProductManagement : Page
+    {
+
+        public ProductManagement()
+        {
+            this.InitializeComponent();
+           
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var inputItem = new ProductManagerModel() { Image = inputImage.Text, ProductName = inputProduct.Text, Description = inputDescription.Text };
+            ProductList.Items.Add(inputItem);
+        }
+
+        private void ProductList_Loaded(object sender, RoutedEventArgs e)
+        {
+            var item = new ProductManagerModel() { Image = "/Assets/picture-box-1.jpg", ProductName = "Burger", Description = "This is Burger" };
+
+            ProductList.Items.Add(item);
+            ProductList.Items.Add(item);
+            ProductList.Items.Add(item);
+        }
+    }
+}
