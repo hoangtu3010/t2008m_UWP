293da6a5fb3d3f2b4804d494de71c90a0329aa25
hoang <hoangtu2378@gmail.com>
2021-10-06 12:10:38 +0700

    update

--- t2008m_UWP/Pages/practical/ProductManagement.xaml.cs
+++ t2008m_UWP/Pages/practical/ProductManagement.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using t2008m_UWP.Models.practical;
+using Windows.Storage;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -33,17 +34,33 @@ namespace t2008m_UWP.Pages.practical
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var inputItem = new ProductManagerModel() { Image = inputImage.Text, ProductName = inputProduct.Text, Description = inputDescription.Text };
+            var inputItem = new ProductManagerModel() { Image = inputImage.SelectedItem.ToString(), ProductName = inputProduct.Text, Description = inputDescription.Text };
             ProductList.Items.Add(inputItem);
         }
 
         private void ProductList_Loaded(object sender, RoutedEventArgs e)
         {
             var item = new ProductManagerModel() { Image = "/Assets/picture-box-1.jpg", ProductName = "Burger", Description = "This is Burger" };
+            var item2 = new ProductManagerModel() { Image = "/Assets/picture-box-1.jpg", ProductName = "Burger", Description = "This is Burger" };
 
             ProductList.Items.Add(item);
             ProductList.Items.Add(item);
             ProductList.Items.Add(item);
+        }
+
+        private void inputImage_Loaded(object sender, RoutedEventArgs e)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"./Assets/");
+
+            FileInfo[] files = dir.GetFiles();
+
+            
+
+            foreach(FileInfo file in files)
+            {
+                var FileName = Path.GetFileName(file.ToString());
+                inputImage.Items.Add("/Assets/" + FileName);
+            }
         }
     }
 }
